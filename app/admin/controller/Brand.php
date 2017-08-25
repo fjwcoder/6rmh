e698b90b455c56ddf6387015c6e51da48f2998df
fjwcoder <fjwcoder@gmail.com>
2017-08-25 08:11:05 +0800

    增加 商品类别管理、商品品牌管理

---
+++ app/admin/controller/Brand.php
@@ -0,0 +1,120 @@
+<?php
+# +-------------------------------------------------------------
+# | CREATE by FJW IN 2017-5-19.
+# | 后台图标管理控制器
+# |
+# | email: fjwcoder@gmail.com
+# +-------------------------------------------------------------
+namespace app\admin\controller;
+use app\common\controller\Manage;
+use app\common\controller\Common;
+use think\Controller;
+use think\Session;
+use think\Cookie;
+use think\Config;
+use think\Request;
+use think\Db;
+use think\Cache;
+
+class Brand extends Manage
+{
+    public function index(){
+
+        $navid = input('navid', 34, 'intval');
+        $nav = adminNav();
+        $keyword = input('keyword', '', 'htmlspecialchars,trim');
+        if(!empty($keyword)){
+            $where = array('name', 'like', "%$keyword%");
+        }else{
+            $where = '1=1';
+        }
+
+        
+        $list = Db::name('mall_brand') ->alias('a') 
+             -> join('mall_category b', 'a.catid=b.id', 'LEFT') 
+             -> field(array('a.id', 'a.title', 'a.logo', 'b.title as category', 'a.status')) 
+             -> where($where) -> order('a.id desc') -> paginate(Config::get('TABLE_LIST_NUM'));
+        $this->assign('list', $list);
+        $header =  ['title'=>'商品管理->商品配置->'.$nav[$navid]['title'], 'icon'=>$nav[$navid]['icon'], 
+            'form'=>'list', 'navid'=>$navid ]; 
+        $this->assign('header', $header);
+        $this->assign('keyword', $keyword?$keyword:'');
+        return $this->fetch();
+    }
+
+
+    public function add(){
+
+        if(request()->isPost()){
+            return $this->dataPost('add');
+        }
+
+        $catid = input('catid', 0, 'intval');//分类ID
+        $navid = input('navid', 34, 'intval');
+        $nav = adminNav();
+        $list = db('mall_category', [], false) -> where(array('status'=>1)) ->order('id_list, sort') -> select();
+        $this->assign('catlist', $list);
+
+        $this->assign('header', ['title'=>'添加品牌', 'icon'=>$nav[$navid]['icon'], 'form'=>'add', 'navid'=>$navid]);
+        return $this->fetch('brand');
+    }
+
+    public function edit(){
+        if(request()->isPost()){
+            return $this->dataPost('edit');
+        }
+        $id = input('id', 0, 'intval');
+        $navid = input('navid', 34, 'intval');
+        $nav = adminNav();
+
+        $list = db('mall_category', [], false) -> where(array('status'=>1)) ->order('id_list, sort') -> select();
+        $this->assign('catlist', $list);
+
+        $result = db('mall_brand', [], false) -> where(array('id'=>$id)) ->find();
+
+        $this->assign('result', $result);
+        $this->assign('header', ['title'=>'编辑品牌', 'icon'=>$nav[$navid]['icon'], 'form'=>'edit', 'navid'=>$navid]);
+        return $this->fetch('brand');
+    }
+    public function dataPost($type){
+        $post = request()->post();
+        unset($post['navid']);
+        foreach($post as $k=>$v){
+            $data[$k] = $v;
+        }
+
+        if($type == 'add'){
+            $check = db('mall_brand') -> where(array('title'=>$data['title'])) -> find(); 
+            if($check){
+                return $this->error('品牌已存在');
+            }
+            $result = db('mall_brand', [], false) -> insert($data);
+        }else{
+            $id = $data['id'];
+            unset($data['id']);
+            $check = db('mall_brand') -> where(array('title'=>$data['title'])) -> find(); 
+            if($check){
+                return $this->error('品牌已存在');
+            }
+            $result = db('mall_brand', [], false) -> where(array('id'=>$id)) ->update($data);
+        }
+
+        if($result){
+            return $this->success('成功', request()->controller().'/index');
+        }else{
+            return $this->error('失败');
+        }
+    }
+
+    
+
+
+    public function del(){
+        return 'del方法';
+    }
+
+    public function delPost(){
+
+    }
+
+}
--- app/admin/controller/Category.php
+++ app/admin/controller/Category.php
@@ -41,13 +41,14 @@ class Category extends Manage
         $navid = input('navid', 32, 'intval');
         $nav = adminNav();
         $keyword = input('post.keyword', '', 'htmlspecialchars,trim');
-        // if(!empty($keyword)){
-        //     $where = ['title', 'like', "%$keyword%"];
-        // } 
-
-        // $list = db('goods_category', [], false)  -> paginate(Config::get('TABLE_LIST_NUM'));
-        // $this->assign('list', $list); 
-        $cat_list = db('goods_category', [], false) -> select();
+        if(!empty($keyword)){
+            //这个还没写好！！！！！！！
+            $id_list = Db::name('mall_category') -> where('title', 'like', $keyword.'%') -> find();
+            $cat_list = Db::name('mall_category') ->where('id_list', 'like', $id_list['id_list'].'%') -> select();
+        }else{
+            $cat_list = Db::name('mall_category') ->order('id_list', 'sort') -> select();
+        }
+        
         if(empty($cat_list)){
             $this->assign('list_tree', '<h3>尚无分类信息</h3>');
         }else{
@@ -70,18 +71,24 @@ class Category extends Manage
             if($cat_list[$i]['pid'] == $fid){
                 $html .= '<dt>';
                 $html .= '<a class="operate-a" data-toggle="collapse" data-parent="#accordion"  href="#collapse-'.$cat_list[$i]['id'].'"><i class="glyphicon-plus"></i></a>';
-                $html .= $cat_list[$i]['title'];
-                $html .= '<a class="operate-a" href="/admin/category/add/navid/32" title="添加分类"><i class="glyphicon glyphicon-plus-sign"></i></a>';
-                $html .= '<a class="operate-a" href="javascript: void(0);"  title="编辑"><i class="	glyphicon glyphicon-edit"></i></a>';
-                $html .= '<a class="operate-a" href="javascript: void(0);"  title="锁定"><i class="	glyphicon glyphicon-eye-open"></i></a>';
+                $html .= '<input class="cat-tree-input text-center" name="category['.$cat_list[$i]['id'].']" value="'.$cat_list[$i]['title'].'"/>';
+                
+                $html .= '<a class="operate-a " href="/admin/category/add/navid/32/id/'.$cat_list[$i]['id'].'" title="添加字分类"><i class="glyphicon glyphicon-plus-sign"></i></a>';
+                $html .= '<a class="operate-a pull-right" href="/admin/category/edit/id/'.$cat_list[$i]['id'].'"  title="编辑"><i class="	glyphicon glyphicon-edit"></i></a>';
+                $html .= '<a class="operate-a pull-right" href="/admin/category/status/id/'.$cat_list[$i]['id'].'"  ';
+                if($cat_list[$i]['status'] == 1){
+                    $html .= 'title="点击锁定" ><i class="	glyphicon glyphicon-eye-open"></i></a>';
+                }else{
+                    $html .= 'title="点击启用" ><i class="	glyphicon glyphicon-eye-close"></i></a>';
+                }
                 $html .= '</dt>';
-                if($cat_list[$i]['isnode'] == 1){
+                if($cat_list[$i]['isnode'] > 0){
                     $html .= '<dd id="collapse-'.$cat_list[$i]['id'].'" class="panel-collapse collapse in">';
                     $html .= '<dl class="panel-body">';
                 }
                 $this->treeBuilder($cat_list[$i]['id']);
 
-                if($cat_list[$i]['isnode'] == 1){
+                if($cat_list[$i]['isnode'] > 0){
                     $html .= '</dl>';
                     $html .= '</dd>';
                 }
@@ -95,9 +102,20 @@ class Category extends Manage
         if(request()->post()){
             return $this->dataPost('add');
         }
+        $pid = input('id', 0, 'intval'); //父类别的id
         $navid = input('navid', 32, 'intval');
         $nav = adminNav();
-        $list = db('goods_category', [], false) -> where(array('status'=>1)) ->order('id_list, sort') -> select();
+
+        $category = Db::name('mall_category') -> where(array('id'=>$pid)) -> find();
+        if($category){
+            $pid_list = $category['id_list'];
+        }else{
+            $pid_list = 0;
+        }
+
+        $this->assign('pid_list', $pid_list);
+
+        $list = db('mall_category', [], false) -> where(array('status'=>1)) ->order('id_list, sort') -> select();
         $this->assign('list', $list);
         $this->assign('header', ['title'=>'添加分类', 'icon'=>$nav[$navid]['icon'], 'form'=>'add', 'navid'=>$navid]);
         return $this->fetch('category');
@@ -110,18 +128,38 @@ class Category extends Manage
             return $this->dataPost('edit');
         }
         $id = input('id', 0, 'intval');
-        $category = db('goods_category', [], false) -> where(array('id'=>$id)) -> find();
         $navid = input('navid', 32, 'intval');
         $nav = adminNav();
-        $list = db('goods_category', [], false) -> where(array('status'=>1)) ->order('sort') -> select();
-        $this->assign('list', $list);
+
+        $category = Db::name('mall_category') -> where(array('id'=>$id)) -> find();
+        if($category && $category['pid'] != 0){
+            $pid_list = Db::name('mall_category') -> where(array('id'=>$category['pid'])) -> value('id_list');
+        }else{
+            $pid_list = 0;
+        }
+        $this->assign('pid_list', $pid_list);
         $this->assign('result', $category);
+        $list = db('mall_category', [], false) -> where(array('status'=>1)) ->order('id_list, sort') -> select();
+        $this->assign('list', $list);
+        
         $this->assign('header', ['title'=>'编辑分类:  【'.$category['title'].'】', 'icon'=>$nav[$navid]['icon'], 'form'=>'edit', 'navid'=>$navid]);
         return $this->fetch('category');
     }
 
     public function dataPost($type=''){
         $post = request()->post();
+
+        // #类别图片上传
+        // if(!empty($_FILES)){
+        //     $upload = uploadImg('images'.DS.'headimg');
+        //     // return dump($upload);
+        //     if($upload['status']){
+        //         $data['headimg'] = $upload['path'];
+        //     }else{
+        //         return $this->error('头像上传失败');
+        //     }
+        // }
+
         foreach($post as $k=>$v){
             $data[$k] = $v;
         }
@@ -131,55 +169,59 @@ class Category extends Manage
                 $data['pid'] = 0;
                 $data['level'] = 1;
                 $data['id_list'] = '';
-                $max = db('goods_category', [], false) ->where(array('pid'=>0, 'level'=>1)) -> max('sort');
+                $max = db('mall_category', [], false) ->where(array('pid'=>0, 'level'=>1)) -> max('sort');
             }else{ 
                 $id_list = explode(',', $data['id_list']);
                 $data['pid'] = $id_list[count($id_list)-1];
                 $data['level'] = count($id_list)+1;
-                $max = db('goods_category', [], false) -> where(array('pid'=>$data['pid'])) ->max('sort');
+                $max = db('mall_category', [], false) -> where(array('pid'=>$data['pid'])) ->max('sort');
             }
+
             $data['sort'] = intval($max)+1;
             $data['addtime'] = intval(time());
             $data['adduser'] = Session::get(Config::get('ADMIN_AUTH_NAME'));
             //获取到自增ID
-            $insert = Db::name('goods_category') -> insert($data); //有bug
-            $id = Db::name('goods_category') ->getLastInsID();
-            $result = db('goods_category', [], false) -> where(array('id'=>$id)) 
+            $insert = Db::name('mall_category') -> insert($data); //有bug
+            $id = Db::name('mall_category') ->getLastInsID();
+            $result = db('mall_category', [], false) -> where(array('id'=>$id)) 
                 -> update(['id_list'=>empty($data['id_list'])?strval($id):$data['id_list'].",$id"]);
-
+            if($result && $data['pid']>0){
+                $result = db('mall_category', [], false) -> where(array('id'=>$data['pid'])) -> setInc('isnode', 1);
+            }
         }else{
             $id = $data['id'];
             unset($data['id']);
+
+            //先把原来的父类别isnode-1
+            $curr_cat = db('mall_category') -> where(['id'=>$id]) -> find();
+            $sub_node = db('mall_category') -> where(['id'=>$curr_cat['pid']]) -> setDec('isnode', 1);
+
             if($data['id_list'] == 0){ //顶级类别
                 $data['pid'] = 0;
                 $data['level'] = 1;
                 $data['id_list'] = '';
-                $max = db('goods_category', [], false) ->where(array('pid'=>0, 'level'=>1)) -> max('sort');
+                $max = db('mall_category', [], false) ->where(array('pid'=>0, 'level'=>1)) -> max('sort');
             }else{
                 $id_list = explode(',', $data['id_list']);
                 $data['pid'] = $id_list[count($id_list)-1];
                 $data['level'] = count($id_list)+1;
-                $max = db('goods_category', [], false) -> where(array('pid'=>$data['pid'])) ->max('sort');
+                $max = db('mall_category', [], false) -> where(array('pid'=>$data['pid'])) ->max('sort');
             }
             $data['sort'] = intval($max)+1;
+            $data['id_list'] = empty($data['id_list'])?strval($id):$data['id_list'].",$id";
 
-            $result = db('goods_category', [], false) -> where(array('id'=>$id)) 
-                -> update(['id_list'=>empty($data['id_list'])?strval($id):$data['id_list'].",$id"]);
+            //这地方最好是写成事务
+            $result = db('mall_category', [], false) -> where(array('id'=>$id)) -> update($data);
+            if($result){
+                $result = db('mall_category', [], false) -> where(array('id'=>$data['pid'])) -> setInc('isnode', 1);
+                
+                $sql = " update keep_mall_category set id_list = replace(id_list, '".$curr_cat['id_list']."', '".$data['id_list']."')";
+                $sql .= " where id_list like '".$curr_cat['id_list']."%' ";
+                Db::query($sql);
+            }
 
-            
-            // #类别图片上传
-            // if(!empty($_FILES)){
-            //     $upload = uploadImg('images'.DS.'headimg');
-            //     // return dump($upload);
-            //     if($upload['status']){
-            //         $data['headimg'] = $upload['path'];
-            //     }else{
-            //         return $this->error('头像上传失败');
-            //     }
-            // }
         }
 
-        
         if($result){
             return $this->success('成功', request()->controller().'/index');
         }else{
@@ -187,73 +229,40 @@ class Category extends Manage
         }
     }
 
-    #用户节点权限设置
-    public function auth(){
-        if(request()->post()){
-            return $this->authPost();
-        }
-
-        $navid = input('navid', 32, 'intval');
-        $nav = adminNav();
-        $user = getUserInfo('goods_category', Session::get(Config::get('USER_KEY')));
-        $this->assign('user', $user);
-        $this->assign('header', ['title'=>'扩展管理->后台用户->'.$nav[$navid]['title'].' 【'.$user['title'].'】', 'form'=>'passCode', 'icon'=>$nav[$navid]['icon'], 'navid'=>$navid]);
-        return $this->fetch('passcode');
-    }
-
-
-
-    #修改密码
-    public function passCode(){
-        if(request()->post()){
-            return $this->passPost();
-        }
-        $navid = input('navid', 32, 'intval');
-        $nav = adminNav();
-        $user = getUserInfo('goods_category', Session::get(Config::get('USER_KEY')));
-        $this->assign('user', $user);
-        $this->assign('header', ['title'=>'扩展管理->后台用户->'.$nav[$navid]['title'].' 【'.$user['title'].'】', 'form'=>'passCode', 'icon'=>$nav[$navid]['icon'], 'navid'=>$navid]);
-        return $this->fetch('passcode');
-    }
-
-    public function passPost(){
-        $post = request() -> post();
-
-        // return dump($post);
-
-        $id = Session::get(Config::get('USER_KEY'));
-        if(empty($post['old-password'])){
-            return $this->error('旧密码不可为空');
-        }
-
-        if(empty($post['password0'])){
-            return $this->error('新密码不可为空');
-        }
-
-        if(empty($post['password1'])){
-            return $this->error('重复密码不可为空');
-        }
-
-        if($post['password0'] !== $post['password1']){
-            return $this->error('新密码输入不一致');
-        }
+    //修改状态
+    public function status(){
+        $id = input('id', 0, 'intval');
+        $result = $this->changeStatus($id);
         
-        $user = getUserInfo('goods_category', $id);
-        // $old_pwd = cryptCode($user['password'], 'DECODE', $user['encrypt']);
-        $old_pwd = cryptCode($post['old-password'], 'ENCODE', substr(md5($post['old-password']), 0, 4));
-        // if($post['old-password'] !== $old_pwd){
-        if($old_pwd !== $user['password']){
-            return $this->error('旧密码错误');
-        }
-
-        $data['encrypt'] = substr(md5($post['password0']), 0, 4);
-        $data['password'] = cryptCode($post['password0'], 'ENCODE', $data['encrypt']);
-        $result = db('goods_category', [], false) -> where(array('id'=>$id)) ->update($data);
-        if($result){
-            session(null);
-            return msg('admin/login/index', '修改成功，请重新登录', 'iframe');
+        if($result['status']){
+            return $this->success($result['content'], request()->controller().'/index');
         }else{
-            return $this->error('修改失败');
+            return $this->error($result['content']);
         }
     }
+
+    public function changeStatus($id=0){
+        $cat = db('mall_category', [], false) -> where(array('id'=>$id)) -> find();
+ 
+        if($cat['status'] == 1){
+
+            $cat_tree = Db::name('mall_category') -> where('id_list', 'like', $cat['id_list'].'%') -> update(['status'=>2]);
+        }else{
+            //由 锁定->启用 需要判断父级状态
+            $in = substr($cat['id_list'], 0, strlen($cat['id_list'])-2);
+            $status = Db::name('mall_category') -> where("id in ($in) and status=2") -> select();
+            if($status){
+                return ['status'=>false, 'content'=>'父级锁定，不可启用']; exit;
+            }
+            $cat_tree = Db::name('mall_category') -> where('id_list', 'like', $cat['id_list'].'%') -> update(['status'=>1]);
+        }
+
+        if($cat_tree){
+            return ['status'=>true, 'content'=>'修改成功'];
+        }else{
+            return ['status'=>false, 'content'=>'修改失败'];
+        }
+    }
+
+
 }
